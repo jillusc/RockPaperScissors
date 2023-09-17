d291958bd18a97d97b47c1cf80c492e0833f79e9
jillusc <>
2023-09-17 20:34:10 +0000

    added player R P S buttons and styled for landscape phone smallest screen size

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+/* Wait for the DOM to finish loading before running the game */
+document.addEventListener("DOMContentLoaded", function() {
+})
+
+
+/* Event listeners for R, P, S buttons*/
+
+
+/* The main game function - called both when the script is first loaded and after the winner has been ascertained */
+function runGame() {
+}
+
+
+/* Get the player's choice of R, P or S */
+function getPlayerChoice() {
+}
+
+
+/* Generate a random choice of R, P or S for the computer */
+function getComputer() {
+}
+
+
+/* Evaluate whether player or computer choice wins */
+function ascertainWinner() {
+}
+
+
+/* Increment scores and losses */
+function incrementScores() {
+}
