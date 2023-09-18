d52c88557c37a312452de557b63fd9a5c79dad3d
jillusc <>
2023-09-18 20:42:53 +0000

    added variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,38 @@
 /* Wait for the DOM to finish loading before running the game */
-document.addEventListener("DOMContentLoaded", function() {
-})
+/* Insert event listeners for R, P, S buttons */
+document.addEventListener("DOMContentLoaded", function () {
+    let buttons = document.getElementsByTagName("button");
+});
 
+/* Variables */
+const choices = ["rock", "paper", "scissors"];
+const buttons = document.querySelectorAll('.btn-choices');
+// const playerChoice ?
+// const computerChoice ?
+let playerScore = 0;
+let computerScore = 0;
 
-/* Insert event listeners for R, P, S buttons*/
-
-
-/* The main game function - called both when the script is first loaded and after the winner has been ascertained */
+/* The main game function - called both when the script is first loaded and after the restart alert */
 function runGame() {
 }
-
 
 /* Get the player's choice of R, P or S */
 function getPlayerChoice() {
 }
 
-
 /* Generate a random choice of R, P or S for the computer */
-function getComputer() {
+function getComputerChoice() {
 }
 
-
-/* Evaluate whether player or computer choice wins */
+/* Determine whether player or computer choice wins */
 function ascertainWinner() {
+    compareChoices(playerChoice, computerChoice);
 }
 
-
-/* Increment scores and losses */
+/* Increment player wins (scores) and computer wins (losses) and display them */
 function incrementScores() {
+}
+
+/* Game end/after 5 rounds add restart alert? */
+function gameEnd() {
 }
