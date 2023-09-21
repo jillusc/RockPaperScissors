2e2714099f7bfdc54bb61453369f45c5faef1025
jillusc <>
2023-09-21 13:47:17 +0000

    added JS for the runGame function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,35 @@
 /* Wait for the DOM to finish loading before running the game */
-/* Insert event listeners for R, P, S buttons */
+/* Get the buttons and insert event listeners for getPlayerChoice (R, P, S) buttons */
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.querySelectorAll(".btn-choices");
-    for (const button of buttons) {
+    let buttons = document.getElementsByClassName("btn-choices");
+    for (let button of buttons) {
         button.addEventListener("click", getPlayerChoice);
     }
+    runGame();
 });
 
 /* Variables */
 const choices = ["rock", "paper", "scissors"];
-const buttons = document.querySelectorAll('.btn-choices');
+const playerChoice = document.getElementById('player-choice-img');
+const computerChoice = document.getElementById('computer-choice-img');
 let playerScore = 0;
 let computerScore = 0;
+const maxScore = 5;
+const winner = ascertainWinner();
+document.getElementById('scores').textContent = playerScore;
+document.getElementById('losses').textContent = computerScore;
+document.getElementById('player-choice-img').style.display = 'none';
+document.getElementById('computer-choice-img').style.display = 'none';
 
 /* The main game function - called both when the script is first loaded and after the restart alert */
 function runGame() {
+    getPlayerChoice();
+    getComputerChoice();
+    const winner = ascertainWinner(playerChoice, computerChoice);
+    incrementScores(winner);
+    if (playerScore === maxScore || computerScore === maxScore) {
+        gameEnd();
+    }
 }
 
 /* Get the player's choice of R, P or S */
@@ -30,6 +45,9 @@ function getPlayerChoice(event) {
         playerChoiceImg.src = 'assets/images/scissors.webp';
     }
     playerChoiceImg.style.display = 'block';
+    const computerChoice = getComputerChoice();
+    const winner = ascertainWinner(playerChoice, computerChoice);
+    updateScores(winner);
 }
 
 /* Generate a random choice of R, P or S for the computer */
@@ -49,13 +67,40 @@ function getComputerChoice() {
 }
 
 /* Determine whether player or computer choice wins */
-function ascertainWinner() {
+function ascertainWinner(playerChoice, computerChoice) {
+    if ((playerChoice === "rock" && computerChoice === "scissors") ||
+        (playerChoice === "paper" && computerChoice === "rock") ||
+        (playerChoice === "scissors" && computerChoice === "paper")) {
+        return "player";
+    } else {
+        return "computer";
+    }
 }
 
 /* Increment player wins (scores) and computer wins (losses) and display them */
-function incrementScores() {
+function incrementScores(winner) {
+    if (winner === "player") {
+        playerScore++;
+    } else if (winner === "computer") {
+        computerScore++;
+    }
+    document.getElementById('scores').textContent = playerScore;
+    document.getElementById('losses').textContent = computerScore;
 }
 
 /* Game end/after 5 rounds add restart alert? */
 function gameEnd() {
+    let winnerText;
+    if (playerScore === maxScore) {
+        winnerText = "You won! Hurrah!";
+    } else {
+        winnerText = "Sorry! The computer won!";
+    }
+
+    alert(winnerText);
+
+    const playAgain = confirm("Do you want to play again?");
+    if (playAgain) {
+        runGame();
+    }
 }
