d3a3f71d56bc65c7c687637817c2ef27973f19b9
jillusc <>
2023-09-24 11:07:07 +0000

    simplified/streamlined the JS removing isWinnerDefined and cleanUp, changed maxScore value

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,125 +1,102 @@
 /* Wait for the DOM to finish loading before running the game */
 /* Get the buttons and insert event listeners for getPlayerChoice (R, P, S) buttons */
+/* Begin the game */
 document.addEventListener("DOMContentLoaded", function () {
-    let buttons = document.getElementsByClassName("btn-choices");
+    const buttons = document.getElementsByClassName("btn-choices");
     for (let button of buttons) {
-        button.addEventListener("click", function (event) {
-            getPlayerChoice(event);
+        button.addEventListener("click", function () {
+            const playerChoice = this.querySelector('img').getAttribute('data-type');
+            runGame(playerChoice);
         });
     }
-    runGame();
 });
 
 /* Variables */
 const choices = ["rock", "paper", "scissors"];
-let playerChoice;
-let computerChoice;
+const maxScore = 6;
 let playerScore = 0;
 let computerScore = 0;
-let winner;
-const maxScore = 5;
-document.getElementById('scores').textContent = playerScore;
-document.getElementById('losses').textContent = computerScore;
-
+const playerChoiceImg = document.getElementById('player-choice-img');
+const computerChoiceImg = document.getElementById('computer-choice-img');
+const scoresElement = document.getElementById('scores');
+const lossesElement = document.getElementById('losses');
 
 /* The main game function - called both when the script is first loaded and after the restart alert */
-function runGame() {
-    winner = ascertainWinner();
+function runGame(playerChoice) {
+    getPlayerChoice(playerChoice);
+    const computerChoice = getComputerChoice();
+    const winner = ascertainWinner(playerChoice, computerChoice);
     incrementScores(winner);
-    if (playerScore === maxScore || computerScore === maxScore) {
-        gameEnd();
-    }
 }
 
 /* Get the player's choice of R, P or S */
-function getPlayerChoice(event) {
-    let playerChoiceTarget = event.target;
-    let playerChoice = playerChoiceTarget.getAttribute('data-type');
-    let playerChoiceImg = document.getElementById('player-choice-img');
-
-    if (playerChoice === 'rock') {
-        playerChoiceImg.src = 'assets/images/rock.webp';
-    } else if (playerChoice === 'paper') {
-        playerChoiceImg.src = 'assets/images/paper.webp';
-    } else if (playerChoice === 'scissors') {
-        playerChoiceImg.src = 'assets/images/scissors.webp';
-    }
+function getPlayerChoice(playerChoice) {
+    playerChoiceImg.src = `assets/images/${playerChoice}.webp`;
     playerChoiceImg.style.display = 'block';
-    getComputerChoice();
-    let winner = ascertainWinner();
-    if (winner != false) {
-        incrementScores(winner);
-    }
+    return playerChoice;
 }
 
 /* Generate a random choice of R, P or S for the computer */
 function getComputerChoice() {
-    let randomIndex = Math.floor(Math.random() * 3);
-    let computerChoice = choices[randomIndex];
-    let computerChoiceImg = document.getElementById('computer-choice-img');
-
-    if (computerChoice === 'rock') {
-        computerChoiceImg.src = 'assets/images/rock.webp';
-    } else if (computerChoice === 'paper') {
-        computerChoiceImg.src = 'assets/images/paper.webp';
-    } else if (computerChoice === 'scissors') {
-        computerChoiceImg.src = 'assets/images/scissors.webp';
-    }
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    const computerChoice = choices[randomIndex];
+    computerChoiceImg.src = `assets/images/${computerChoice}.webp`;
     computerChoiceImg.style.display = 'block';
+    return computerChoice;
 }
 
 /* Determine whether player or computer choice wins */
-function ascertainWinner() {
-    if (playerChoice === computerChoice) { return false; }
-    if ((playerChoice === "rock" && computerChoice === "scissors") ||
+function ascertainWinner(playerChoice, computerChoice) {
+    if (
+        (playerChoice === "rock" && computerChoice === "scissors") ||
         (playerChoice === "paper" && computerChoice === "rock") ||
-        (playerChoice === "scissors" && computerChoice === "paper")) {
+        (playerChoice === "scissors" && computerChoice === "paper")
+    ) {
         return "player";
+    } else if (playerChoice === computerChoice) {
+        return;
     } else {
         return "computer";
     }
 }
 
 /* Increment player wins (scores) and computer wins (losses) and display them */
-function incrementScores(winner) {
-    if (winner === "player") {
-        playerScore++;
-    } else if (winner === "computer") {
-        computerScore++;
+function incrementScores(winner, playerChoice, computerChoice) {
+    if (winner === "player") playerScore++;
+    else if (winner === "computer") computerScore++;
+
+    scoresElement.textContent = playerScore;
+    lossesElement.textContent = computerScore;
+
+    if (playerScore === maxScore || computerScore === maxScore) {
+        gameEnd(playerChoice, computerChoice);
     }
-    document.getElementById('scores').textContent = playerScore;
-    document.getElementById('losses').textContent = computerScore;
-    if (isWinnerDefined() === true) {
-        gameEnd();
-    }
-}
-function isWinnerDefined() {
-    if (playerScore === maxScore || computerScore === maxScore) return true;
-    return false;
 }
 
 /* Game end = after 5 rounds add restart alert */
-function gameEnd() {
+function gameEnd(_playerChoice, _computerChoice) {
     let winnerText;
     if (playerScore === maxScore) {
         winnerText = "You won! Hurrah!";
     } else {
         winnerText = "Sorry! The computer won!";
     }
-
     alert(winnerText);
 
-    let playAgain = confirm("Play again?");
+    const playAgain = confirm("Play again?");
     if (playAgain) {
-        cleanUp();
-    }
-
-    function cleanUp() {
         playerScore = 0;
         computerScore = 0;
         document.getElementById('scores').textContent = playerScore;
         document.getElementById('losses').textContent = computerScore;
-        computerChoice.src = 'assets/images/blank.webp';
-        playerChoice.src = 'assets/images/blank.webp';
+
+        const playerChoiceImg = document.getElementById('player-choice-img');
+        const computerChoiceImg = document.getElementById('computer-choice-img');
+
+        playerChoiceImg.src = 'assets/images/blank.webp';
+        computerChoiceImg.src = 'assets/images/blank.webp';
+
+        location.reload(); // I found this method of refreshing the page
+                           // at https://www.w3schools.com/jsref/met_loc_reload.asp
     }
 }
