fbdca92e6aedeb811adf202ded0be9a243fb45f2
jillusc <>
2023-09-24 12:34:56 +0000

    final versions

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,7 +54,7 @@ function ascertainWinner(playerChoice, computerChoice) {
     ) {
         return "player";
     } else if (playerChoice === computerChoice) {
-        return;
+        return "tie";
     } else {
         return "computer";
     }
